35e7818d804cffc10b8132f46d57a6d9196b749a
Aseel <aseelk.bader@gmail.com>
2022-12-04 09:29:56 +0200

    Fourth: Tuples:- 1: Reverse the tuple 2: Access value 20 from the tuple 3: Unpack the tuple into 4 variables

---
+++ main.py
@@ -0,0 +1,23 @@
+#Fourth: Tuples:-
+#Reverse the tuple
+tuple1R = (10, 20, 30, 40, 50)
+tuple1R =tuple1R[::-1]
+print(tuple1R)
+
+#################################################
+#Access value 20 from the tuple
+tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
+#value=tuple1[0]="orange"
+value=tuple1[1]
+#value=tuple1[2]=(5, 15, 25)
+valuee=value[1]
+print(valuee)
+
+#################################################
+#Unpack the tuple into 4 variables
+tuple11 = (10, 20, 30, 40)
+a,b,c,d =tuple11
+print(a)
+print(b)
+print(c)
+print(d)
